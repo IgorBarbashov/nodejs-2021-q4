29feb687c163379f338a314258bedec3972abd6f
Igor Barbashov <bis200705@gmail.com>
2021-11-14 23:06:16 +0300

    feat: add cli files paths validation

--- task_01_ciphering_cli_tool/index.js
+++ task_01_ciphering_cli_tool/index.js
@@ -1,6 +1,11 @@
 const { validateCliOptions } = require('./validation/cliOptions');
 const { validateCliConfigValue } = require('./validation/cliConfigValue');
+const { validateFilePath } = require('./validation/cliFilesPaths');
+const { cliOptions: { cliInputOption, cliOutputOption } } = require('./constants/cliParameters');
 
 const cliParameters = process.argv.slice(2);
+
 const isCliOptionsValid = validateCliOptions(cliParameters);
-const isConfigValueValid = validateCliConfigValue(cliParameters);
+const isCliConfigValueValid = validateCliConfigValue(cliParameters);
+const isCliInputFilePathValid = validateFilePath(cliInputOption, cliParameters);
+const isCliOutputFilePathValid = validateFilePath(cliOutputOption, cliParameters);
--- task_01_ciphering_cli_tool/validation/cliOptions.js
+++ task_01_ciphering_cli_tool/validation/cliOptions.js
@@ -25,5 +25,6 @@ const validateCliOptions = (cliParameters) => {
 };
 
 module.exports = {
+    isOptionExistsInCli,
     validateCliOptions
 };
