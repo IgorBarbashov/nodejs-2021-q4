6db8fffb52de24ef20c2d99c6079897ee55a4a24
Igor Barbashov <bis200705@gmail.com>
2021-11-15 17:32:49 +0300

    feat: add custom readable and writable streams

--- task_01_ciphering_cli_tool/streams/outputStream.js
+++ task_01_ciphering_cli_tool/streams/outputStream.js
@@ -1,8 +1,43 @@
-const { createWriteStream } = require('fs');
+const { open, write, close } = require('fs');
+const { Writable } = require('stream');
 const { isOptionExistsInCli } = require('../validation/cliOptions');
 const { cliOptions: { cliOutputOption } } = require('../constants/cliParameters');
 const { getCliOptionValue } = require('../utils/getCliOptionValue');
 const { OPEN_FILE_MODE } = require('../constants/streams');
+const { errorHandler, OpenFileError } = require('../errors/index');
+
+class CustomOutputStream extends Writable {
+    constructor(filePath) {
+        super();
+        this.filePath = filePath;
+        this.fd = null;
+    };
+
+    _construct(callback) {
+        open(this.filePath, OPEN_FILE_MODE.WRITABLE_STREAM, (err, fd) => {
+            if (err) {
+                errorHandler(new OpenFileError(err.message));
+            } else {
+                this.fd = fd;
+                callback();
+            }
+        })
+    };
+
+    _write(chunk, _, callback) {
+        write(this.fd, chunk, callback);
+    };
+
+    _destroy(error, callback) {
+        if (this.fd) {
+            close(this.fd, (err) => {
+                callback(err || error);
+            })
+        } else {
+            callback(error);
+        }
+    };
+};
 
 const createOutputStream = (cliParameters) => {
     const isOutputOptionExistsInCli = isOptionExistsInCli(cliOutputOption, cliParameters);
@@ -10,7 +45,7 @@ const createOutputStream = (cliParameters) => {
         return process.stdout;
     }
     const outputFilePath = getCliOptionValue(cliOutputOption, cliParameters);
-    return createWriteStream(outputFilePath, { flags: OPEN_FILE_MODE.WRITABLE_STREAM });
+    return new CustomOutputStream(outputFilePath);
 };
 
 module.exports = {
